6ca1a180e02760f7ac6f42905cbbba0e0d5b285b
縫嶋慧深 <tefu@tefuko.local>
2020-02-17 15:08:20 +0900

    help

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const msgStyle = {
+  color: 'blue'
+};
+
+const App = () => {
+  return (
+    <>
+      <h1 style={msgStyle}>はじめてのReact</h1>
+    </>
+  );
+}
+
+export default App;
